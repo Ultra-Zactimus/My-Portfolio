03c88f3b2f4f729556e8e230515fcbdbe12a2a56
Ultra Zacky <ultrazacky@gmail.com>
2022-09-21 14:48:08 -0700

    add Portfolio component

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,40 @@
-
+import Image from "next/image";
+import myWebpages from "./index";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {myWebpages.map(({ id, image, title, desc, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item--image">
+                <Image 
+                  src={image} 
+                  alt={title} 
+                  className="img"
+                />
+              </div>
+              <h3>{title}</h3>
+              <small>{desc}</small>
+              <div className="portfolio__item--cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
