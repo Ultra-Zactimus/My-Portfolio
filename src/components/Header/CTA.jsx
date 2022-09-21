f78d31d2b3473e070949512bf400ace9c0e99f5b
Ultra Zacky <ultrazacky@gmail.com>
2022-09-21 14:22:14 -0700

    add CTA component

---
+++ src/components/Header/CTA.jsx
@@ -0,0 +1,13 @@
+
+
+const CTA = () => {
+  return (
+    <div className="header__cta">
+      <a className="btn btn-primary" href="#contact">
+        Let&lsquo;s Work Together!
+      </a>
+    </div>
+  );
+};
+
+export default CTA;
