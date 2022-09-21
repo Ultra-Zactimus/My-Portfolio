ba49419992e4aa20f085443f0700f9e9f4af18c1
Ultra Zacky <ultrazacky@gmail.com>
2022-09-21 12:13:54 -0700

    add index for exporting all components

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import About from './About/About';
+import Contact from './Contact/Contact';
+import Experience from './Experience/Experience';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Nav from './Nav/Nav';
+import Portfolio from './Portfolio/Portfolio';
+
+
+export {
+  About,
+  Contact,
+  Experience,
+  Footer,
+  Header,
+  Nav,
+  Portfolio
+}
