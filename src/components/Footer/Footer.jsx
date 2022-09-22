72c9f209987bf09313b1b75b8818cf957174f8ef
Ultra Zacky <ultrazacky@gmail.com>
2022-09-22 13:57:59 -0700

    remove unused sections from footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,13 +20,7 @@ const Footer = () => {
           <a href="#experience">Experience</a>
         </li>
         <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
           <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#testimonials">Testimonials</a>
         </li>
         <li>
           <a href="#contact">Contact</a>
